a50ec49d7eb151a7ee44c40b3d78f879bc6ac4ea
m1n-q <m1n_q@naver.com>
2022-05-23 08:28:27 +0900

    chore: add some util funcs
    trim(string str, char* delims):
        trim one of delims from front and back of str.
    extractHeader(string content):
        extract header-field as map<string, string> from raw content.
    lowerize(const string&):
        return new lowerized string

--- utils.hpp
+++ utils.hpp
@@ -6,7 +6,7 @@
 /*   By: mishin <mishin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 15:03:17 by mishin            #+#    #+#             */
-/*   Updated: 2022/05/06 17:49:55 by mishin           ###   ########.fr       */
+/*   Updated: 2022/05/23 08:27:05 by mishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,5 +73,59 @@ string	getExt(const string& path)
 {
 	string ext = path.substr(path.rfind('.')+1, path.length()-1);
 	return ext;
+}
+
+
+inline std::string rtrim(std::string s, const char* t)
+{
+    s.erase(s.find_last_not_of(t) + 1);
+    return s;
+}
+
+inline std::string ltrim(std::string s, const char* t)
+{
+    s.erase(0, s.find_first_not_of(t));
+    return s;
+}
+
+inline std::string trim(std::string s, const char* t)
+{
+    return ltrim(rtrim(s, t), t);
+}
+
+map<string, string>	extractHeader(string content)	//NOTE: what if line endswith \r\n
+{
+	const char*			ws		= " \t\n\r\f\v";
+	string::size_type	pStart	= 0;
+	string::size_type	pEnd	= string::npos;
+	string::size_type	pDelim	= 0;
+	string				line;
+	map<string, string>	ret;
+
+
+	while ((pEnd = content.find('\n', pStart)) != string::npos)
+	{
+		line = content.substr(pStart, pEnd-pStart);
+		if (line.empty() || line == "\r")	break;
+
+		pDelim = line.find_first_of(":");
+		if (pDelim == string::npos)	continue;
+		ret[trim(line.substr(0,pDelim), ws)] = trim(line.substr(pDelim+1), ws);
+
+		pStart = pEnd + 1;
+	}
+	return ret;
+}
+
+string lowerlize(const string& s)
+{
+	string				ret(s);
+	string::iterator	it;
+	string::iterator	ite = ret.end();
+
+	for (it = ret.begin(); it < ite; it++)
+		if (isupper(*it))
+			*it = tolower(*it);
+	return ret;
 }
 #endif
