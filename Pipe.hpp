99c290b3bc66b32e128abb79378ff64c89888ae8
m1n-q <m1n_q@naver.com>
2022-05-28 18:16:28 +0900

    fix: add pid, status to class Pipe

--- Pipe.hpp
+++ Pipe.hpp
@@ -1,18 +1,21 @@
 #ifndef PIPE_HPP
 # define PIPE_HPP
 # include "IStream.hpp"
+#include <sys/_types/_pid_t.h>
 
 class Pipe : public IStream
 {
 public:
-	string content;
+	string	content;
+	pid_t	pid;
+	int		status;
 
 public:
 	Pipe()
-	: IStream(), content() {};
+	: IStream(), content(), pid(0), status(0) {};
 
-	Pipe( int fd )
-	:IStream(fd), content() {};
+	Pipe( int fd, pid_t p )
+	:IStream(fd), content(), pid(p), status(0) {};
 
 	Pipe( const Pipe& src )
 	: IStream(src), content(src.content) {}
