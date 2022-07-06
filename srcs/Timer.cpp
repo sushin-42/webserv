663925725cc57397670646efbb99261b29b3a1f6
m1n-q <m1nq.dev@gmail.com>
2022-07-06 17:30:56 +0900

    chore: fix indentation

--- srcs/Timer.cpp
+++ srcs/Timer.cpp
@@ -1,55 +1,55 @@
 #include "Timer.hpp"
 
 
-// /**========================================================================
-// * @                           Constructors
-// *========================================================================**/
+/**========================================================================
+* @                           Constructors
+*========================================================================**/
 
 Timer::Timer()
 : timeoutPool(), min(-1, numeric_limits<time_t>::max())	{}
 Timer::~Timer() {}
 
-// /**========================================================================
-// * #                          member functions
-// *========================================================================**/
-	void	Timer::monitor()
+/**========================================================================
+* #                          member functions
+*========================================================================**/
+void	Timer::monitor()
+{
+	PollSet::_Map::iterator it = POLLSET->pollMap.begin();
+	PollSet::_Map::iterator ite = POLLSET->pollMap.end();
+
+	pair<int, time_t> min = make_pair(-1, numeric_limits<time_t>::max() );
+
+	time_t	now = time(NULL);
+	time_t	timeout = -1;
+	time_t	lastActive = -1;
+
+	time_t	elapsedTime;
+	time_t	remainingTime;
+	Stream* stream;
+
+	for (;it != ite; it++)
 	{
-		PollSet::_Map::iterator it = POLLSET->pollMap.begin();
-		PollSet::_Map::iterator ite = POLLSET->pollMap.end();
+		stream = it->second.second;
+		if (CONVERT(stream, ServerSocket))
+			continue ;
+		if ((timeout = (stream)->getTimeOut()) < 0)	// No limit
+			continue ;
+		if ((lastActive = (stream)->getLastActive()) < 0)
+			continue ;
 
-		pair<int, time_t> min = make_pair(-1, numeric_limits<time_t>::max() );
-
-		time_t	now = time(NULL);
-		time_t	timeout = -1;
-		time_t	lastActive = -1;
-
-		time_t	elapsedTime;
-		time_t	remainingTime;
-		Stream* stream;
-
-		for (;it != ite; it++)
+		/*--------------------------------------------*/
+		elapsedTime = difftime(now, lastActive);
+		remainingTime = difftime(timeout, elapsedTime);
+		if (remainingTime <= 0)
 		{
-			stream = it->second.second;
-			if (CONVERT(stream, ServerSocket))
-				continue ;
-			if ((timeout = (stream)->getTimeOut()) < 0)	// No limit
-				continue ;
-			if ((lastActive = (stream)->getLastActive()) < 0)
-				continue ;
-
-			/*--------------------------------------------*/
-			elapsedTime = difftime(now, lastActive);
-			remainingTime = difftime(timeout, elapsedTime);
-			if (remainingTime <= 0)
-			{
-				LOGGING(Timer,  UL("%d") RED( "TIMEOUT %lus"), stream->getFD(), timeout);
-				timeoutPool.push_back(stream->getFD());
-			}
-			else if (remainingTime <= min.second)
-			{
-				min.first = stream->getFD();
-				min.second = remainingTime;
-			}
+			LOGGING(Timer,  UL("%d") RED( "TIMEOUT %lus"), stream->getFD(), timeout);
+			timeoutPool.push_back(stream->getFD());
 		}
-		this->min = min;
+		else if (remainingTime <= min.second)
+		{
+			min.first = stream->getFD();
+			min.second = remainingTime;
+		}
 	}
+	this->min = min;
+}
